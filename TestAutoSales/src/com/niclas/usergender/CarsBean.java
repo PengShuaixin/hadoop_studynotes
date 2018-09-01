9418dc0ce932485bb86e69d93f1c6cd65d8117d0
PengShuaixin <42090337+PengShuaixin@users.noreply.github.com>
2018-09-01 17:34:16 +0800

    car_class_2.2

---
+++ TestAutoSales/src/com/niclas/usergender/CarsBean.java
@@ -0,0 +1,79 @@
+package com.niclas.usergender;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.WritableComparable;
+/*
+ * 2.2所有权、车辆类型、品牌封装成对象，实现WritableComparable接口作为key
+ */
+public class CarsBean implements WritableComparable<CarsBean>{
+	private String ownership;//所有权9
+	private String type;//车辆类型8
+	private String brand;//品牌7
+	
+	public void set(String ownership, String type, String brand) {
+		this.ownership = ownership;
+		this.type = type;
+		this.brand = brand;
+	}
+	
+	public CarsBean() {
+		super();
+	}
+
+	public String getOwnership() {
+		return ownership;
+	}
+	public void setOwnership(String ownership) {
+		this.ownership = ownership;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	@Override
+	public void write(DataOutput out) throws IOException {
+		// TODO Auto-generated method stub
+		out.writeUTF(ownership);
+		out.writeUTF(type);
+		out.writeUTF(brand);
+	}
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		// TODO Auto-generated method stub
+		this.ownership = in.readUTF();
+		this.type = in.readUTF();
+		this.brand = in.readUTF();
+	}
+	@Override
+	public int compareTo(CarsBean o) {
+		// TODO Auto-generated method stub
+		if(this.ownership.equals(o.getOwnership())) {
+			if (this.brand.equals(o.getBrand())) {
+				return this.type.compareTo(o.getType());
+			}
+		}
+		if(this.ownership.equals(o.getOwnership())) {
+			if (this.type.equals(o.getType())) {
+				return this.brand.compareTo(o.getBrand());
+			}
+		}
+		return this.ownership.compareTo(o.getOwnership());
+		
+	}
+	@Override
+	public String toString() {
+		return ownership + "\t" + type + "\t" + brand;
+	}
+	
+}
---
+++ TestAutoSales/src/com/niclas/usergender/CountCarsSales.java
@@ -0,0 +1,85 @@
+package com.niclas.usergender;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+/*
+ * 2.2统计的车的所有权、车辆类型和品牌的分布
+ */
+public class CountCarsSales {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		// TODO Auto-generated method stub
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf);
+		//设置Mapper和Reducer
+		job.setMapperClass(CountMapper.class);
+		job.setReducerClass(CountReduce.class);
+		//设置Map的输出类型
+		job.setMapOutputKeyClass(CarsBean.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		//设置Reduce输出类型
+		job.setOutputKeyClass(CarsBean.class);
+		job.setOutputValueClass(IntWritable.class);
+		//设置job文件的输入
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		//设置job文件的输出
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		//设置自定义的comparator类 
+		//job.setGroupingComparatorClass();
+		//指定自定义数据分区器
+		//job.setPartitionerClass();
+		//指定相应“分区”数量的reducetask
+		//job.setNumReduceTasks(5);
+		//提交job执行
+		//job.submit();
+		boolean falag = job.waitForCompletion(true);
+		System.out.println(falag?"执行成功":"执行失败");		
+		
+	}
+
+	public static class CountMapper extends Mapper<LongWritable, Text, CarsBean, IntWritable> {
+		CarsBean carsBean = new CarsBean();
+		IntWritable intWritable = new IntWritable(1);
+		@Override
+		protected void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			// TODO Auto-generated method stub
+			String[] values = value.toString().split("\t");
+			if (values.length >= 20) {
+				String ownership = values[9];
+				String type = values[8];
+				String brand = values[7];
+				carsBean.set(ownership, type, brand);
+				context.write(carsBean, intWritable);
+
+			}
+			
+		}
+		
+	}
+	
+	public static class CountReduce extends Reducer<CarsBean, IntWritable, CarsBean, IntWritable> {
+
+		@Override
+		protected void reduce(CarsBean key, Iterable<IntWritable> value,
+				Context context) throws IOException, InterruptedException {
+			// TODO Auto-generated method stub
+			int count = 0;
+			for (IntWritable intWritable : value) {
+				count += intWritable.get();
+			}
+			context.write(key, new IntWritable(count));
+		}
+		
+	}
+}
